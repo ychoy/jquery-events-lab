1f57d0414fe5ce030c72a55df5a18cf7c380b4a9
ychoy <yanyinchoy@gmail.com>
2017-01-31 14:46:44 -0800

    updated base.js file so text reads 'go'

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -3,10 +3,9 @@
 */
 
 $(document).ready(function(){
-  console.log( "The page says:", $("h1").text() );
+  console.log( $("h1").text() );
+  $("h1").text("Go!");
 });
-
-$("h1").text("Go!");
 
 if ( $("h1").text() === "Go!" ){
   console.log( "The page says:", $("h1").text() );
@@ -14,3 +13,4 @@ if ( $("h1").text() === "Go!" ){
 } else {
   console.log( 'Can you make it say "Go!"?' );
 }
+
