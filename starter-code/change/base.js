e8380a9ede23b34f8f4672ea745139556574ba2b
ychoy <yanyinchoy@gmail.com>
2017-01-31 16:01:57 -0800

    updated change base.js file to include change function to modify the input total

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -2,6 +2,15 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $("input.add").change(function handleChange(event) {
+    var first = $("input#left").val();
+    var second = $("input#right").val();
+    var firstVal = parseInt(first) || 0;
+    var secondVal = parseInt(second) || 0;
+    var total = firstVal + secondVal;
+    $("#total").val(total);
+  });
+
   $("#time").text( Date.now() );
 
 })
